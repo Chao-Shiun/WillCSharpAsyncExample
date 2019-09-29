1a01569fb5f6b2fa847c3a18a17e4b361dd25198
Charles <e4366.tw@gmail.com>
2019-09-29 11:26:36 +0800

    C007內容調整

--- CSharpAsync/C007.使用執行緒做到非同步處理作業/Program.cs
+++ CSharpAsync/C007.使用執行緒做到非同步處理作業/Program.cs
@@ -12,7 +12,7 @@ namespace C007.使用執行緒做到非同步處理作業
         static void Main(string[] args)
         {
             Console.WriteLine($"建立兩個執行緒物件");
-            Thread thread1 = new Thread(() =>
+            Task task1 = Task.Run(() =>
             {
                 Console.WriteLine($"執行緒1 的 ID={Thread.CurrentThread.ManagedThreadId} ( 輸出 X )");
                 Thread.Sleep(900);
@@ -22,7 +22,8 @@ namespace C007.使用執行緒做到非同步處理作業
                 }
                 //Console.WriteLine();
             });
-            Thread thread2 = new Thread(() =>
+            //thread1.IsBackground = true;
+            Task task2 = Task.Run(() =>
             {
                 Console.WriteLine($"執行緒2 的 ID={Thread.CurrentThread.ManagedThreadId} ( 輸出 - )");
                 Thread.Sleep(900);
@@ -34,16 +35,23 @@ namespace C007.使用執行緒做到非同步處理作業
             });
             Console.WriteLine($"啟動執行兩個執行緒");
 
-            thread1.Start();
-            thread2.Start();
+            //thread1.Start();
+            //thread2.Start();
 
-            thread1.Join(); // 等待 執行緒1 執行完畢
-            thread2.Join(); // 等待 執行緒2 執行完畢
+            //thread1.Join(); // 等待 執行緒1 執行完畢
+            //thread2.Join(); // 等待 執行緒2 執行完畢
 
+            //task1.Wait();
+            //task2.Wait();
             Console.WriteLine();
 
-            //Console.WriteLine("Press any key to continue...");
-            //Console.ReadKey();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            /*
+             這個範例是在說如果你不Join或wait thread，那麼當執行到Console.ReadKey()你把它按掉
+             main thread就會被結束，其它在這main thread底下的thread就會一起被kill
+             */
         }
     }
 }
